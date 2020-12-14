28daa80a7ec8a68aba65e66ebf26735456f40538
newlyedward <newlyedward@qq.com>
2020-12-14 23:16:38 +0800

    QA_Charts_Frame 可视化K线，成交量和持仓量的基本图形

--- QUANTAXIS/QAUtil/QAPlot.py
+++ QUANTAXIS/QAUtil/QAPlot.py
@@ -23,13 +23,19 @@
 # SOFTWARE.
 import os
 import webbrowser
+from functools import lru_cache
 
 try:
     from pyecharts import Kline
 except:
-    from pyecharts.charts import Kline
+    import pandas as pd
+    import numpy as np
+    from pyecharts import options as opts
+    from pyecharts.commons.utils import JsCode
+    from pyecharts.charts import Kline, Line, Bar, Grid
 
 from QUANTAXIS.QAUtil.QALogs import QA_util_log_info
+from QUANTAXIS.QAData.base_datastruct import _quotation_base
 
 
 """
@@ -41,6 +47,149 @@ from QUANTAXIS.QAUtil.QALogs import QA_util_log_info
 
 主要是画DataStruct的k线图, DataStruct加指标的图,以及回测框架的回测结果的图
 """
+
+INIT_OPTS = opts.InitOpts(width='1360px', height='800px', page_title='QUANTAXIS')
+
+
+class QA_Charts_Frame:
+    """
+    可视化数据，按主流软件图形，
+        上部分为K线图，可以叠加各种指标,
+        中间为成交量图，期货叠加持仓量
+        下部分为指标幅图，
+    """
+
+    def __init__(self, ds: _quotation_base):
+        """
+
+        :param ds: 只有一个品种的ds
+        """
+        self.code = ds.code[0]
+        self.ds = ds.select_code(self.code)  # 多个品种只返回第一个
+        self.ohlc = self.ds.data.loc[:, ['open', 'close', 'low', 'high']].values.tolist()
+
+        self.inst, self.freq = self.ds.type.split('_')
+        if self.freq == 'day':
+            self.datetime = self.ds.date.map(lambda x: str(x).split(' ')[0]).to_list()
+        else:
+            self.datetime = np.array(self.ds.datetime.map(str)).tolist()
+
+        k_count = 72  # 一次显示7根K线，计算缩放的起始比例
+        self.range_start = int(100 - k_count / len(self.datetime) * 100)
+
+    @property
+    @lru_cache()
+    def k_line(self):
+        # K线主图
+        print(self.ds.code)
+        k_line = (
+            Kline(init_opts=INIT_OPTS)
+                .add_xaxis(xaxis_data=self.datetime)
+                .add_yaxis(
+                series_name=self.code,
+                y_axis=self.ohlc,
+                itemstyle_opts=opts.ItemStyleOpts(
+                    color="#ef232a",
+                    color0="#14b143",
+                    border_color="#ef232a",
+                    border_color0="#14b143",
+                ),
+                markpoint_opts=opts.MarkPointOpts(
+                    data=[
+                        opts.MarkPointItem(type_="max", name="最大值"),
+                        opts.MarkPointItem(type_="min", name="最小值"),
+                    ]
+                ),
+            )
+                .set_global_opts(
+                title_opts=opts.TitleOpts(title=self.code, pos_left="0"),
+                xaxis_opts=opts.AxisOpts(
+                    type_="category",
+                    is_scale=True,
+                    boundary_gap=False,
+                    axisline_opts=opts.AxisLineOpts(is_on_zero=False),
+                    splitline_opts=opts.SplitLineOpts(is_show=False),
+                    split_number=20,
+                    min_="dataMin",
+                    max_="dataMax",
+                ),
+                yaxis_opts=opts.AxisOpts(
+                    is_scale=True, splitline_opts=opts.SplitLineOpts(is_show=True)
+                ),
+                tooltip_opts=opts.TooltipOpts(trigger="axis", axis_pointer_type="line"),
+                datazoom_opts=[
+                    opts.DataZoomOpts(
+                        is_show=True, type_="inside", range_start=self.range_start, range_end=100
+                    ),
+                    opts.DataZoomOpts(
+                        is_show=True, pos_bottom="-1%", range_start=self.range_start, range_end=100
+                    ),
+                ],
+
+            )
+        )
+        return k_line
+
+    @property
+    @lru_cache()
+    def vol_bar(self):
+        # 成交量辅图
+        if self.inst == 'future':
+            vol = self.ds.volume.map(lambda x: int(x * 100)).tolist()
+        else:
+            vol = self.ds.volume.map(int).tolist()
+
+        vol_bar = (
+            Bar(init_opts=INIT_OPTS)
+            .add_xaxis(xaxis_data=self.datetime)
+            .add_yaxis(
+                series_name="Volume",
+                y_axis=vol,
+                label_opts=opts.LabelOpts(is_show=False),
+            )
+                .set_global_opts(
+                xaxis_opts=opts.AxisOpts(
+                    type_="category",
+                ),
+                tooltip_opts=opts.TooltipOpts(trigger="axis", axis_pointer_type="line"),
+                datazoom_opts=[
+                    opts.DataZoomOpts(
+                        is_show=True, type_="inside", range_start=self.range_start, range_end=100
+                    ),
+                    opts.DataZoomOpts(
+                        is_show=True, pos_bottom="-1%", range_start=self.range_start, range_end=100
+                    ),
+                ],
+            )
+        )
+        return vol_bar
+
+    @property
+    @lru_cache()
+    def pos_line(self):
+        pos = self.ds.position.map(int).tolist()
+        pos_line = (
+            Line(init_opts=INIT_OPTS)
+            .add_xaxis(xaxis_data=self.datetime)
+            .add_yaxis(
+                series_name="Position",
+                y_axis=pos,
+                label_opts=opts.LabelOpts(is_show=False),
+                is_symbol_show=False,
+            )
+                .set_global_opts(
+                tooltip_opts=opts.TooltipOpts(trigger="axis", axis_pointer_type="line"),
+                datazoom_opts=[
+                    opts.DataZoomOpts(
+                        is_show=True, type_="inside", range_start=self.range_start, range_end=100
+                    ),
+                    opts.DataZoomOpts(
+                        is_show=True, pos_bottom="-1%", range_start=self.range_start, range_end=100
+                    ),
+                ],
+            )
+        )
+        return pos_line
 
 
 def plot_datastruct(_quotation_base, code=None):
@@ -121,3 +270,4 @@ def QA_plot_save_html(pic_handle, path, if_open_web):
     else:
         pass
     QA_util_log_info('The Pic has been saved to your path: %s' % path)
+
